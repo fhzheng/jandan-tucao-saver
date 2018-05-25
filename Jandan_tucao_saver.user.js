b33b1374c233e261ac317b5bad79e68c90055aa8
fhzheng <mtdwss@gmail.com>
2018-05-25 12:30:55 +0800

    修改版本号为2.0.0

--- Jandan_tucao_saver.user.js
+++ Jandan_tucao_saver.user.js
@@ -14,7 +14,7 @@
 
 // @require     https://cdn.bootcss.com/vue/2.4.2/vue.min.js
 // @description save jandan.net's tucao
-// @version     1.10.2
+// @version     2.0.0
 // @grant       none
 /*jshint esversion: 6 */
 // ==/UserScript==
